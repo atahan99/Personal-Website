ebb648c13767de61e68b7e5a1dc3905b739e21e7
atahan99 <atahankucuk@gmail.com>
2022-01-18 17:19:44 -0500

    change of dimensions

--- pages/resume.js
+++ pages/resume.js
@@ -18,13 +18,13 @@ const Resume = () => (
         data='/resume.pdf'
         type="application/pdf"
         width="100%"
-        height="100%"
+        height="1000"
       >
 
         <iframe
           src='/resume.pdf'
           width="100%"
-          height="100%"
+          height="1000"
         >
         <p>This browser does not support PDF!</p>
         </iframe>
